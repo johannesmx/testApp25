89abb07bb4ac9446a049a20404b5f434611ade9d
johannes <johannes.muljana@ait.nsw.edu.au>
2025-10-10 11:30:06 +1100

    added firebase config

---
+++ config/config.dist.ts
@@ -0,0 +1,9 @@
+// put api keys values and save as config.ts
+export const firebaseConfig = {
+    apiKey: "project_api_key",
+    authDomain: "project_authDomain",
+    projectId: "projectId",
+    storageBucket: "storageBucket_domain",
+    messagingSenderId: "SenderId",
+    appId: "appId"
+};
