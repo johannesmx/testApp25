402cf4f27b714f4984a068acd07232942e19d72e
johannes <johannes.muljana@ait.nsw.edu.au>
2025-10-17 09:59:58 +1100

    adding signup form

--- app/(tabs)/test.tsx
+++ app/(tabs)/test.tsx
@@ -1,15 +1,17 @@
 import { ThemedView } from "@/components/themed-view";
 import { ThemedText } from "@/components/themed-text";
-import { Link } from "expo-router"
+import { Pressable } from "react-native";
+import { Link, useRouter } from "expo-router"
 
 
 export default function TestScreen() {
-    return(
+    const router = useRouter()
+    return (
         <ThemedView>
             <ThemedText>Test screen</ThemedText>
-            <Link href={"../"}>
+            <Pressable onPress={ ()=> router.dismissAll() }>
                 <ThemedText>Sign out</ThemedText>
-            </Link>
+            </Pressable>
         </ThemedView>
     )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,56 @@
 import { ThemedView } from "@/components/themed-view";
 import { ThemedText } from "@/components/themed-text";
 import { Link } from "expo-router"
+import { View, TextInput, StyleSheet, Pressable } from 'react-native'
+import { useState, useEffect } from 'react'
 
 export default function AuthScreen() {
     return (
-        <ThemedView>
-            <ThemedText>Authenticate</ThemedText>
-            <Link href={"/(tabs)"}>
-                <ThemedText>Login</ThemedText>
-            </Link>
+        <ThemedView style={styles.container}>
+            <View style={styles.form}>
+                <ThemedText style={styles.title} >Sign up for an account</ThemedText>
+                <ThemedText>Email</ThemedText>
+                <TextInput style={styles.input} />
+                <ThemedText>Password</ThemedText>
+                <TextInput style={styles.input} secureTextEntry={true}/>
+                <Pressable style={styles.button }>
+                    <ThemedText style={styles.buttonText}>Sign up</ThemedText>
+                </Pressable>
+            </View>
         </ThemedView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    form: {
+        marginHorizontal: 20,
+        marginTop: 50,
+    },
+    title: {
+        textAlign: "center",
+        fontSize: 18,
+    },
+    input: {
+        borderWidth: 2,
+        borderColor: "#666666",
+        borderStyle: "solid",
+        borderRadius: 5,
+        color: "#FFFFFF",
+        padding: 5,
+        marginBottom: 10,
+    },
+    button: {
+        borderWidth: 2,
+        borderColor: "#666666",
+        borderStyle: "solid",
+        borderRadius: 5,
+        padding: 5,
+        marginVertical: 10,
+    },
+    buttonText: {
+        textAlign: "center",
+    },
+})
