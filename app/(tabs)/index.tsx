fdada4e42e1f7b5983e97e80c1e9eaf563896242
johannes <johannes.muljana@ait.nsw.edu.au>
2025-10-31 11:20:18 +1100

    getting data and displaying in home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,34 +1,62 @@
-import { Platform, StyleSheet } from 'react-native';
+import { Platform, StyleSheet, FlatList } from 'react-native';
 import { useContext, useState, useEffect } from 'react';
 
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { DataContext } from '@/contexts/DataContext';
+import { Item } from '@/interfaces/ItemInterface'
 
 import { collection, Firestore, getDocs } from 'firebase/firestore';
 
 export default function HomeScreen() {
-  const [data,setData] = useState([])
+  const [data,setData] = useState<Item[]>([])
+  const [loaded,setLoaded] = useState<boolean>(false)
   // initialise firestore through context
   const db:any = useContext( DataContext )
 
   const getData = async () => {
     const fsdata = await getDocs( collection(db, "shared") )
-    let items = []
+    let items:Item[] = []
     fsdata.forEach( (fsdoc) => {
-      let item = fsdoc.data()
+      let item:any = fsdoc.data()
       item.id = fsdoc.id
       items.push( item )
     })
     setData( items )
+    console.log("data")
   }
 
+  interface ListItemProps {
+    title:string
+    id:string
+  }
+
+  const ListItem = (props:ListItemProps) => {
+    return (
+      <ThemedView>
+        <ThemedText>{ props.title }</ThemedText>
+      </ThemedView>
+    )
+  }
+
+  useEffect( () => {
+    //getData()
+    if(loaded==false) {
+      getData()
+      setLoaded(true)
+    }
+  },[])
 
   return (
     <SafeAreaView style={styles.container}>
       <ThemedView>
         <ThemedText>Home Screen</ThemedText>
+        <FlatList
+          data={data}
+          renderItem={ ({item}) => <ListItem title={ item.name } id={ item.id }/> }
+          keyExtractor={ item => item.id }
+        />
       </ThemedView>
     </SafeAreaView>
   );
