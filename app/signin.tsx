d6c1765b862377fc74d50a86deebfdd8872feee0
johannes <johannes.muljana@ait.nsw.edu.au>
2025-11-13 23:19:54 +1100

    update to signout flow

--- app/signin.tsx
+++ app/signin.tsx
@@ -53,7 +53,7 @@ export default function AuthScreen() {
     const signUp = () => {
         signInWithEmailAndPassword(auth, email, password)
             .then((response) => console.log(response))
-            .catch((error) => console.log(error))
+            .catch((error) => console.log(error.message))
     }
 
     return (
